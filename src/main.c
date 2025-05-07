4bfe0a9721c68eb0710563f156aea15ef9f70d74
Yago Guirra <yagoguirra@hotmail.com>
2025-05-06 23:35:29 -0300

    ultimas coisas implementadas

--- src/main.c
+++ src/main.c
@@ -22,14 +22,16 @@ typedef enum {
     PISCANDO
 } EstadoSemaforo;
 
-
 void setup();
+
 void task_mode(void *pvParameters);
 void task_semaphore(void *pvParameters);
 void task_buzzer(void *pvParameters);
+void task_matrix(void *pvParameters);
+void task_display(void *pvParameters);
+
 void buzzer_beep(uint8_t gpio, uint16_t freq, uint16_t duration_ms);
 bool delay_interrompivel(int tempo_ms, bool modo_original);
-
 
 display dp;
 
@@ -40,7 +42,6 @@ volatile EstadoSemaforo estado_atual = VERDE;
 int main() {
     stdio_init_all();
     setup();
-
 
     xTaskCreate (
         task_mode, 
@@ -69,13 +70,30 @@ int main() {
         NULL
     );
 
+    xTaskCreate (
+        task_matrix, 
+        "Matrix",
+        configMINIMAL_STACK_SIZE,
+        NULL,
+        1,
+        NULL
+    );
+    xTaskCreate (
+        task_display, 
+        "Display",
+        configMINIMAL_STACK_SIZE,
+        NULL,
+        1,
+        NULL
+    );
+
     vTaskStartScheduler();
     panic_unsupported();
 }
 
 void setup() {
-    // display_init(&dp);
-    // matrix_init(MATRIX_LED_PIN);
+    display_init(&dp);
+    matrix_init(MATRIX_LED_PIN);
     button_init();
     buzzer_init(BUZZER_A_PIN);
     led_init(LED_BLUE_PIN);
@@ -85,11 +103,10 @@ void setup() {
 
 bool delay_interrompivel(int tempo_ms, bool modo_original) {
     for (int i = 0; i < tempo_ms; i += 100) {
-        // Se o modo atual mudou, interrompe a espera
         if (noturn_mode != modo_original) return false;
         vTaskDelay(pdMS_TO_TICKS(100));
     }
-    return true;  // terminou sem interrupção
+    return true;
 }
 
 void task_mode(void *pvParameters) {
@@ -101,7 +118,9 @@ void task_mode(void *pvParameters) {
                 noturn_mode = !noturn_mode;
                 estado_atual = noturn_mode ? PISCANDO : VERDE;
             } else if (button_state == BUTTON_B) {
-                // display_shutdown(&dp);
+                display_shutdown(&dp);
+                matrix_clear();
+                matrix_update();
                 reset_usb_boot(0, 0);
             }
             button_clear_event();
@@ -142,6 +161,98 @@ void task_semaphore(void *pvParameters) {
     }
 }
 
+void task_display(void *pvParameters) {
+    bool blink_on = true;
+
+    while (1) {
+        display_clear(&dp);
+
+        display_draw_rectangle(0, 0, 20, 20, estado_atual == VERDE, true, &dp);
+        display_draw_rectangle(0, 22, 20, 42, (estado_atual == AMARELO || estado_atual == PISCANDO), true, &dp);
+        display_draw_rectangle(0, 44, 20, 63, estado_atual == VERMELHO, true, &dp);
+
+        if (estado_atual == VERDE) {
+            display_draw_string(25, 4, "VERDE", true, &dp);
+        } else if (estado_atual == AMARELO) {
+            display_draw_string(25, 26, "AMARELO", true, &dp);
+        } else if (estado_atual == VERMELHO) {
+            display_draw_string(25, 48, "VERMELHO", true, &dp);
+        } else if (estado_atual == PISCANDO) {
+            if (blink_on) {
+                display_draw_string(25, 26, "AMARELO", true, &dp);
+            }
+        }
+
+        display_draw_string(70, 0, noturn_mode ? "NOTURNO" : "NORMAL", true, &dp);
+
+        display_update(&dp);
+
+        if (noturn_mode) {
+            blink_on = !blink_on;
+            if (!delay_interrompivel(500, true)) continue;
+        } else {
+            if (!delay_interrompivel(100, false)) continue;
+        }
+    }
+}
+
+
+void task_matrix(void *pvParameters) {
+    bool on = true;
+
+    while (1) {
+        matrix_clear();
+
+        switch (estado_atual) {
+
+            case VERDE:
+                for (int i = 1; i < 4; i++) {
+                    for (int j = 1; j < 4; j++) {
+                        matrix_set_led_xy(j, i, COLOR_RGB(0, 60, 0));
+                    }
+                }
+                matrix_update();
+                vTaskDelay(pdMS_TO_TICKS(100));
+                break;
+
+            case AMARELO:
+                for (int i = 1; i < 4; i++) {
+                    for (int j = 1; j < 4; j++) {
+                        matrix_set_led_xy(j, i, COLOR_RGB(60, 60, 0));
+                    }
+                }
+                matrix_update();
+                vTaskDelay(pdMS_TO_TICKS(100));
+                break;
+
+            case VERMELHO:
+                for (int i = 1; i < 4; i++) {
+                    for (int j = 1; j < 4; j++) {
+                        matrix_set_led_xy(j, i, COLOR_RGB(60, 0, 0));
+                    }
+                }
+                matrix_update();
+                vTaskDelay(pdMS_TO_TICKS(100));
+                break;
+
+            case PISCANDO:
+                if (on) {
+                    for (int i = 1; i < 4; i++) {
+                        for (int j = 1; j < 4; j++) {
+                            matrix_set_led_xy(j, i, COLOR_RGB(120, 120, 0));
+                        }
+                    }
+                } else {
+                    matrix_clear();
+                }
+                matrix_update();
+                on = !on;
+                vTaskDelay(pdMS_TO_TICKS(500));
+                break;
+        }
+    }
+}
+
 
 void buzzer_rtos_beep(uint8_t gpio, uint16_t freq, uint16_t duration_ms) {
     buzzer_turn_on(gpio, freq);
@@ -153,28 +264,24 @@ void task_buzzer(void *pvParameters) {
     while (1) {
         switch (estado_atual) {
             case VERDE:
-                buzzer_rtos_beep(BUZZER_A_PIN, 1000, 100);  // 100ms
-                // Espera o resto do tempo (900ms) para completar 1s total
-                vTaskDelay(pdMS_TO_TICKS(900));
+                buzzer_rtos_beep(BUZZER_A_PIN, 1000, 500);
+                vTaskDelay(pdMS_TO_TICKS(500));
                 break;
 
             case AMARELO:
                 for (int i = 0; i < 4; i++) {
-                    buzzer_rtos_beep(BUZZER_A_PIN, 1000, 100);  // 100ms
-                    // Espera 50ms após cada beep para totalizar 150ms por ciclo
+                    buzzer_rtos_beep(BUZZER_A_PIN, 1000, 100);
                     vTaskDelay(pdMS_TO_TICKS(50));
                 }
                 break;
 
             case VERMELHO:
-                buzzer_rtos_beep(BUZZER_A_PIN, 1000, 500);  // 500ms
-                // Espera 1500ms para completar os 2s totais
+                buzzer_rtos_beep(BUZZER_A_PIN, 1000, 500);
                 vTaskDelay(pdMS_TO_TICKS(1500));
                 break;
 
             case PISCANDO:
-                buzzer_rtos_beep(BUZZER_A_PIN, 1000, 100);  // 100ms
-                // Espera 1900ms restantes
+                buzzer_rtos_beep(BUZZER_A_PIN, 1000, 100);
                 vTaskDelay(pdMS_TO_TICKS(1900));
                 break;
         }
